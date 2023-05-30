e19b36c328d0424af60fe69cb24d7e818b85353e
NHOY <NHOY@2210DR000919>
2023-05-31 01:45:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp44/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp44
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number;
+            int numberStartExample = 0;
+            int stepInExample = 3;
+            int stepInExample2 = 5;
+
+            number = rand.Next(0, 101);
+
+            Console.WriteLine("Ваше число:" + number);
+
+            Console.WriteLine("Сумма кратных числу " + stepInExample + ":");
+            for (int i = numberStartExample; i <= number; i += stepInExample) 
+            { 
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Сумма кратных числу " + stepInExample2 + ":");
+            for (int j = numberStartExample; j <= number; j += stepInExample2)
+            {
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
